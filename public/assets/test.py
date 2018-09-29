b5de48a36f4d6a4fcb8c1d5d7ad01cdf301206c7
gregg <gmicay123@gmail.com>
2018-09-29 12:53:33 +0800

    log upload UI done

--- public/assets/test.py
+++ public/assets/test.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 import random
+import sys
 
 def main():
-	for x in range(10):
-		print (random.randint(1, 101))
+	sample_array = [1, 2, 3, 4, 5, 6, 7, 8]
+	print(sample_array)
+	# for x in range(10):
+	# 	print (random.randint(1, 101))
+	sys.stdout.flush()
 
 if __name__ == '__main__':
 	main()
